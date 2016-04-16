aa70c5c3f8be498046516c9f28076ba19389ee10
Nicolas Faure <faure_n@epita.fr>
2016-04-16 17:04:45 +0200

    Basic libgdx project

---
+++ android/src/fr/fbb/divisr/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fr.fbb.divisr;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.fbb.divisr.Divisr;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Divisr(), config);
+	}
+}
